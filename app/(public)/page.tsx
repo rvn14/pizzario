7d9cbf32f44cb69fa7da4c5ae9885641be72f9b2
rvn14 <dasunadithya123@gmail.com>
2025-03-12 22:26:32 +0530

    model setup

---
+++ app/(public)/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import dynamic from 'next/dynamic'
+import Scene from '../components/Scene'
+
+
+
+
+const Home = () => {
+  return (
+    <div className='w-full h-screen bg-gray-50'>
+        <Scene/>
+    </div>
+  )
+}
+
+export default Home
